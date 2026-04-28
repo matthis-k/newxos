dae8ed3344c4f0a3ea309deb52526ce0475e5a1d
matthis-k <matthis.kaelble@gmail.com>
2026-04-28 10:14:34 +0200

    feat(git): configure git tools for matthisk

--- modules/development/git.nix
+++ modules/development/git.nix
@@ -6,6 +6,18 @@
       programs.git = {
         enable = true;
         package = pkgs.gitFull;
+        settings = {
+          user.name = "matthis-k";
+          user.email = "matthis.kaelble@gmail.com";
+
+          pull.rebase = false;
+          merge.conflictstyle = "diff3";
+          init.defaultBranch = "main";
+          core.editor = "nvim";
+        };
       };
+
+      programs.gh.enable = true;
+      programs.lazygit.enable = true;
     };
 }
